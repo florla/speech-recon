ec60b9002ae438150b10e0204a6bf0c037dbd748
florla <feliciamlbrownwork@gmail.com>
2023-10-26 14:34:05 -0400

    speech recognition functioning

--- script.js
+++ script.js
@@ -11,7 +11,20 @@ let p = document.createElement('p');
 words.append(p);
 
 recognition.addEventListener('result', e => {
-    console.log(e);
+    // console.log(e);
+    const transcript = Array.from (e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+    p.textContent = transcript;
+
+    if (e.results[0].isFinal){
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    
 });
+
+recognition.addEventListener('end',recognition.start);
 
 recognition.start();
